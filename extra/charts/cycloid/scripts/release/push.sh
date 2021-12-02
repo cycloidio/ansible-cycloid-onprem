ffba81ad5417630038f8f9fc9c4ae84de91786a0
Steve Durrheimer <steve.durrheimer@cycloid.io>
2021-12-02 17:06:12 +0100

    helm: improve scripts/release/push.sh Usability by adding AWS ENVVARs

--- extra/charts/cycloid/scripts/release/push.sh
+++ extra/charts/cycloid/scripts/release/push.sh
@@ -2,6 +2,10 @@
 
 set -e
 
+export AWS_ACCESS_KEY_ID=$(vault read -field=access_key secret/cycloid/aws)
+export AWS_SECRET_ACCESS_KEY=$(vault read -field=secret_key secret/cycloid/aws)
+export AWS_DEFAULT_REGION=eu-west-1
+
 VERSION=$(changie latest)
 
 echo -e "\e[36m# $0 > switching to master brabnch and make sure it's up-to-date\e[0m"
