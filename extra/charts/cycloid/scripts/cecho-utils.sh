21b3f1df3a4052008655f0d230cc83dfe919cc29
Frédéric BARRAS HAMPEL <frederic.hampel@cycloid.io>
2025-01-14 14:19:54 +0100

    func: make deploy scripts POSIX sh compatible

--- extra/charts/cycloid/scripts/cecho-utils.sh
+++ extra/charts/cycloid/scripts/cecho-utils.sh
@@ -1,4 +1,5 @@
-#!/bin/bash
+#!/usr/bin/bash
+# shellcheck shell=sh
 
 # INFO: this script come from https://github.com/cycloidio/docker-cycloid-toolkit/blob/develop/scripts/cecho-utils
 
@@ -15,85 +16,80 @@
 # -n or --noline directs the system not to print a new line after the content.
 # Last argument is the message to be printed.
 
-cecho () {
-    current_xtrace_enabled=${-//[^x]/}
-    set +x
-    xtrace_enabled="${xtrace_enabled:-$current_xtrace_enabled}"
+cecho() {
+  current_xtrace_enabled=${-//[^x]/}
+  set +x
+  xtrace_enabled="${xtrace_enabled:-$current_xtrace_enabled}"
 
-    declare -A colors;
-    colors=(\
-        ['black']='\E[0;47m'\
-        ['red']='\E[0;31m'\
-        ['green']='\E[0;32m'\
-        ['yellow']='\E[0;33m'\
-        ['blue']='\E[0;34m'\
-        ['magenta']='\E[0;35m'\
-        ['cyan']='\E[0;36m'\
-        ['white']='\E[0;37m'\
-    );
+  defaultMSG="No message passed."
+  defaultColor="black"
+  defaultNewLine=true
 
-    local defaultMSG="No message passed.";
-    local defaultColor="black";
-    local defaultNewLine=true;
-
-    while [[ $# -gt 1 ]];
-    do
-    key="$1";
+  while [ $# -gt 1 ]; do
+    key="$1"
 
     case $key in
-        -c|--color)
-            color="$2";
-            shift;
-        ;;
-        -n|--noline)
-            newLine=false;
-        ;;
-        *)
-            # unknown option
-        ;;
+    -c | --color)
+      color="$2"
+      shift
+      ;;
+    -n | --noline)
+      newLine=false
+      ;;
+    *)
+      # unknown option
+      ;;
     esac
-    shift;
-    done
+    shift
+  done
 
-    message=${1:-$defaultMSG};   # Defaults to default message.
-    color=${color:-$defaultColor};   # Defaults to default color, if not specified.
-    newLine=${newLine:-$defaultNewLine};
+  message=${1:-$defaultMSG} # Defaults to default message.
+  case ${color-$defaultColor} in
+  black) color='\E[0;47m' ;;
+  red) color='\E[0;31m' ;;
+  green) color='\E[0;32m' ;;
+  yellow) color='\E[0;33m' ;;
+  blue) color='\E[0;34m' ;;
+  magenta) color='\E[0;35m' ;;
+  cyan) color='\E[0;36m' ;;
+  white) color='\E[0;37m' ;;
+  esac
 
-    echo -en "${colors[$color]}";
-    echo -en "$message";
-    if [ "$newLine" = true ] ; then
-        echo;
-    fi
-    tput -T xterm-256color sgr0; #  Reset text attributes to normal without clearing screen.
+  newLine=${newLine:-$defaultNewLine}
 
-    if [[ -n "$xtrace_enabled" ]]; then set -x; else set +x; fi
-    return;
+  printf "$color%s" "$message"
+  if [ "$newLine" = true ]; then
+    echo
+  fi
+  tput -T xterm-256color sgr0 #  Reset text attributes to normal without clearing screen.
+
+  if [ -n "$xtrace_enabled" ]; then set -x; else set +x; fi
+  return
 }
-pwarning () {
-    xtrace_enabled=${-//[^x]/}
-    set +x
-    cecho -c 'yellow' "$@";
-    if [[ -n "$xtrace_enabled" ]]; then set -x; else set +x; fi
-}
-
-perror () {
-    xtrace_enabled=${-//[^x]/}
-    set +x
-    cecho -c 'red' "$@";
-    if [[ -n "$xtrace_enabled" ]]; then set -x; else set +x; fi
-}
-
-pinfo () {
-    xtrace_enabled=${-//[^x]/}
-    set +x
-    cecho -c 'blue' "$@";
-    if [[ -n "$xtrace_enabled" ]]; then set -x; else set +x; fi
+pwarning() {
+  xtrace_enabled=${-//[^x]/}
+  set +x
+  cecho -c 'yellow' "$@"
+  if [ -n "$xtrace_enabled" ]; then set -x; else set +x; fi
 }
 
-psuccess () {
-    xtrace_enabled=${-//[^x]/}
-    set +x
-    cecho -c 'green' "$@";
-    if [[ -n "$xtrace_enabled" ]]; then set -x; else set +x; fi
+perror() {
+  xtrace_enabled=${-//[^x]/}
+  set +x
+  cecho -c 'red' "$@"
+  if [ -n "$xtrace_enabled" ]; then set -x; else set +x; fi
 }
 
+pinfo() {
+  xtrace_enabled=${-//[^x]/}
+  set +x
+  cecho -c 'blue' "$@"
+  if [ -n "$xtrace_enabled" ]; then set -x; else set +x; fi
+}
+
+psuccess() {
+  xtrace_enabled=${-//[^x]/}
+  set +x
+  cecho -c 'green' "$@"
+  if [ -n "$xtrace_enabled" ]; then set -x; else set +x; fi
+}
--- extra/charts/cycloid/scripts/generate-random-passwords.sh
+++ extra/charts/cycloid/scripts/generate-random-passwords.sh
@@ -1,11 +1,14 @@
-#!/bin/bash
+#! /usr/bin/env bash
+# shellcheck shell=sh
+
 set -x
-DIR="$(dirname $(readlink -f $0))"
+DIR="$(dirname "$(readlink -f "$0")")"
 OUTPUT_DIR="$DIR/.out"
 
-source $DIR/cecho-utils.sh
+# shellcheck source=./cecho-utils.sh
+. "$DIR/cecho-utils.sh"
 
-[ ! -d $OUTPUT_DIR ] && mkdir $OUTPUT_DIR
+[ ! -d "$OUTPUT_DIR" ] && mkdir "$OUTPUT_DIR"
 
 export VALUES_CUSTOM_YAML="${VALUES_CUSTOM_YAML:-./values.custom.yaml}"
 
@@ -20,47 +23,47 @@ PATTERNS="
 "
 set +x
 if [ ! -f "$VALUES_CUSTOM_YAML" ]; then
-    perror "$0 > $VALUES_CUSTOM_YAML file not found"
-    exit 1
+  perror "$0 > $VALUES_CUSTOM_YAML file not found"
+  exit 1
 else
-    psuccess "$0 > $VALUES_CUSTOM_YAML successfully found"
+  psuccess "$0 > $VALUES_CUSTOM_YAML successfully found"
 fi
 
 if ! command -v uuidgen /dev/null; then
-    perror "$0 > uuidgen command not found. Please install it. For exammple apt-get install uuid-runtime"
-    exit 1
+  perror "$0 > uuidgen command not found. Please install it. For exammple apt-get install uuid-runtime"
+  exit 1
 fi
 
-injectKey () {
-    pinfo "  ... Replacing $pattern"
-    key_file=$1
-    pattern=$2
-    content=$(sed 's/^/      /' $key_file)
-    
-    sed -i -e "/$pattern/{
+injectKey() {
+  pinfo "  ... Replacing $pattern"
+  key_file=$1
+  pattern=$2
+  content=$(sed 's/^/      /' "$key_file")
+
+  echo "$content" | sed -i -e "/$pattern/{
         r /dev/stdin
         d
-    }" $VALUES_CUSTOM_YAML <<< "$content"
+    }" "$VALUES_CUSTOM_YAML"
 }
 
 pwarning "$0 > Generate and replace passwords"
 for pattern in $PATTERNS; do
-    password="$(< /dev/urandom tr -dc A-Za-z0-9- | head -c32)"
-    pinfo "  ... Replacing $pattern"
-    sed -i "s/$pattern/$password/g" $VALUES_CUSTOM_YAML
+  password="$(</dev/urandom tr -dc A-Za-z0-9- | head -c32)"
+  pinfo "  ... Replacing $pattern"
+  sed -i "s/$pattern/$password/g" "$VALUES_CUSTOM_YAML"
 done
 
 pattern="##backend-jwtKey1##"
-password="$(< /dev/urandom tr -dc A-Za-z0-9 | head -c64)"
+password="$(</dev/urandom tr -dc A-Za-z0-9 | head -c64)"
 randomuuid=$(uuidgen)
 pinfo "  ... Replacing $pattern"
-sed -i "s/$pattern/${randomuuid}:${password}/g" $VALUES_CUSTOM_YAML
+sed -i "s/$pattern/${randomuuid}:${password}/g" "$VALUES_CUSTOM_YAML"
 
 pwarning "$0 > Generate keys using ssh-keygen"
 
-test -f $OUTPUT_DIR/concourse_session_signing_key || ssh-keygen -C '' -N '' -t rsa -b 4096 -m PEM -f $OUTPUT_DIR/concourse_session_signing_key
-test -f $OUTPUT_DIR/concourse_tsa_host_key || ssh-keygen -C '' -N '' -t rsa -b 4096 -m PEM -f $OUTPUT_DIR/concourse_tsa_host_key
-test -f $OUTPUT_DIR/concourse_worker_key || ssh-keygen -C '' -N '' -t rsa -b 4096 -m PEM -f $OUTPUT_DIR/concourse_worker_key
+test -f "$OUTPUT_DIR/concourse_session_signing_key" || ssh-keygen -C '' -N '' -t rsa -b 4096 -m PEM -f "$OUTPUT_DIR/concourse_session_signing_key"
+test -f "$OUTPUT_DIR/concourse_tsa_host_key" || ssh-keygen -C '' -N '' -t rsa -b 4096 -m PEM -f "$OUTPUT_DIR/concourse_tsa_host_key"
+test -f "$OUTPUT_DIR/concourse_worker_key" || ssh-keygen -C '' -N '' -t rsa -b 4096 -m PEM -f "$OUTPUT_DIR/concourse_worker_key"
 
 injectKey "$OUTPUT_DIR/concourse_session_signing_key" "##concourse-secrets-sessionSigningKey##"
 injectKey "$OUTPUT_DIR/concourse_tsa_host_key" "##concourse-secrets-hostKey##"
